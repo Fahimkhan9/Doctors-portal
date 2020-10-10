4a1cc6f064555f000d75aeeb72e712d2172c557b
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-10-10 23:19:43 +0600

    styled make-appointment-section

--- src/Components/Home/MakeAppointment.js
+++ src/Components/Home/MakeAppointment.js
@@ -3,18 +3,21 @@ import doctor from '../../images/5790-removebg.png'
 import './MakeAppointment.css'
 function MakeAppointment() {
     return (
-       <section  className="make-appointment-container d-flex justify-content-center mb-5  ">
-<div className="row w-75 make-appointment ">
-    <div className="col-md-5">
-        <img style={{height:"400px"}} src={doctor} alt=""/>
-    </div>
-    <div className="col-md-7 d-flex flex-column justify-content-end align-items-start ">
-<h5>Appointment</h5>
-<h3>Make an Appointment Today</h3>
-<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea ipsam quos assumenda laborum, dolorem necessitatibus.</p>
+
+       <section className="make-appointment mb-5">
+           <div className="container ">
+               <div className="row">
+                   <div className="col-md-5 d-none d-md-block">
+<img src={doctor}  alt=""/>
+                   </div>
+                   <div className="col-md-7 py-5 text-white">
+<h5 className="text-uppercase" style={{color:"#1CC7C1"}}>Appointment</h5>
+<h1 className="my-4">Make An Appointment</h1>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, facere est, officiis quis, necessitatibus error voluptas quibusdam perspiciatis at voluptates porro. Aliquam, maxime ullam. Fugit tempore ipsam maxime nihil commodi mollitia nam minus perspiciatis voluptatibus minima nisi maiores quos similique, quas culpa, eveniet voluptates distinctio ea nobis saepe provident? Vitae tempora veniam rerum doloribus consequuntur error atque provident pariatur tenetur</p>
 <button className="btn btn-primary">Learn More</button>
-    </div>
-</div>
+                   </div>
+               </div>
+           </div>
        </section>
     )
 }
